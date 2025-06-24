7a2f175202f6783b99e09fd788b9c02a74345b0f
SilvaNeto29 <silva.netocataguases@gmail.com>
2025-06-23 23:50:27 -0300

    fix: incluindo start do servidor php na integração

--- tests/Feature/AuthApiTest.php
+++ tests/Feature/AuthApiTest.php
@@ -23,7 +23,7 @@ class AuthApiTest extends TestCase
     protected function setUp(): void
     {
         $this->client = new Client([
-            'base_uri' => 'http://localhost',
+            'base_uri' => 'http://localhost:8000',
             'http_errors' => false,
         ]);
     }
